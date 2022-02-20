3a9b65bd0b5d3b31599427657f08e31e063e5c81
André Luiz da Silva <andreluizmicro@gmail.com>
2022-02-20 13:38:24 -0300

    feat: :zap: Created relations
    Created relations entity video

--- src/main/java/com/fullcycle/FCCatalogo/domain/entity/Video.java
+++ src/main/java/com/fullcycle/FCCatalogo/domain/entity/Video.java
@@ -2,15 +2,21 @@ package com.fullcycle.FCCatalogo.domain.entity;
 
 import java.text.DecimalFormat;
 import java.util.Calendar;
+import java.util.List;
 import java.util.UUID;
+import java.util.ArrayList;
 
 public class Video extends BaseEntity {
+
     private String title;
     private String description;
     private Integer yearLaunched;
     private Boolean opened;
     private String rating;
     private Float duration;
+    private List<Category> categories = new ArrayList<Category>();
+    private List<Genre> genres = new ArrayList<Genre>();
+    private List<CastMember> castMembers = new ArrayList<CastMember>();
 
     public Video() {
     }
@@ -104,5 +110,71 @@ public class Video extends BaseEntity {
 
     public Float getDuration() {
         return duration;
+    }
+
+    public List<Category> getCategories() {
+        return this.categories;
+    }
+
+    public void setCategories(List<Category> categories) {
+        if (categories == null)
+            throw new IllegalArgumentException("categories is marked non-null but is null");
+        this.categories = categories;
+    }
+
+    public List<Genre> getGenres() {
+        return this.genres;
+    }
+
+    public void setGenres(List<Genre> genres) {
+        if (genres == null)
+            throw new IllegalArgumentException("genres is marked non-null but is null");
+        this.genres = genres;
+    }
+
+    public List<CastMember> getCastMembers() {
+        return this.castMembers;
+    }
+
+    public void setCastMembers(List<CastMember> castMembers) {
+        if (castMembers == null)
+            throw new IllegalArgumentException("castMembers is marked non-null but is null");
+        this.castMembers = castMembers;
+    }
+
+    public void addCategory(Category category) {
+        if (category == null)
+            throw new IllegalArgumentException("categoriy is marked non-null but is null");
+        this.categories.add(category);
+    }
+
+    public void removeCategory(Category category) {
+        if (category == null)
+            throw new IllegalArgumentException("categoriy is marked non-null but is null");
+        this.categories.removeIf(c -> this.categories.contains(category));
+    }
+
+    public void addGenre(Genre genre) {
+        if (genre == null)
+            throw new IllegalArgumentException("genre is marked non-null but is null");
+        this.genres.add(genre);
+    }
+
+    public void removeGenre(Genre genre) {
+        if (genre == null)
+            throw new IllegalArgumentException("genre is marked non-null but is null");
+        this.genres.removeIf(c -> this.genres.contains(genre));
+    }
+
+    public void addCastMembers(CastMember member) {
+        if (member == null)
+            throw new IllegalArgumentException("member is marked non-null but is null");
+        this.castMembers.add(member);
+    }
+
+    public void removeCastMembers(CastMember member) {
+        if (member == null)
+            throw new IllegalArgumentException("member is marked non-null but is null");
+        this.castMembers.removeIf(c -> this.castMembers.contains(member));
     }
 }
